708ea3793c040e9701001f2712f60fe1ec9f0fa2
AarooshRBLX <164038746+Aarooshsaifanboy322@users.noreply.github.com>
2024-04-11 19:33:55 +0600

    Cracker Files

---
+++ Cracker_exe/main.py
@@ -0,0 +1,13 @@
+# Cracker
+# By Aarooshcoding33
+import time
+
+while True:
+    Crack = str(input("Enter your file name: "))
+    if Crack == "":
+        print("Error")
+        continue
+    else:
+        print("Cracking...")
+        time.sleep(14)
+        print("Successfuly cracked.")
